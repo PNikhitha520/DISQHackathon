062c005de8a020a089d5b2038cbd53a3c17376e8
PNikhitha520 <palagiri.nikhitha@yahoo.com>
2016-11-27 18:54:20 +0530

    CODE339

---
+++ nn.php
@@ -0,0 +1,9 @@
+
+<?php
+ 
+$a= $_GET['first_name'];
+
+$b=$_GET['email'];
+echo $a;
+echo $b;
+?>
---
+++ submit.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Form Example</title>
+</head>
+<body>
+<ul><li><b>Name:</b> <?php echo $_GET['yourname']; ?></li>
+<li><b>Address:</b> <?php echo $_GET['adress']; ?></li>
+<li><b>Phone:</b> <?php echo $_GET['phone']; ?></li></ul>
+</body>
+</html>
